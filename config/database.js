425e13043951fbb958286b55bb32e83f3bd3419f
Omary Ally <omaryally.em@gmail.com>
2022-12-22 22:43:31 -0500

    db connection string

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING, {
+    const conn = await mongoose.connect("mongodb+srv://omasa:omasa1652@grocery.gakjw9r.mongodb.net/amuaFintech?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
